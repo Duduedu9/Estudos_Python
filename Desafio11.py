ddba0cc0529d3a788a08dfb774202a2e8ee54e2d
Eduardo Bizerra <Eduardo_Bizerra@hotmail.com>
2023-01-24 12:01:44 -0300

    Initial commit

---
+++ Desafio11.py
@@ -0,0 +1,6 @@
+A=float(input('Qual a altura da parede?: '))
+L=float(input('Qual a largura da parede?: '))
+AT=L*A
+QT=AT/2
+print('A altura é {}, a largura é {} a area total é {}'.format(A, L, AT))
+print('A quantidade de tinta necessaria para pintar a parede é {}'.format(QT))
