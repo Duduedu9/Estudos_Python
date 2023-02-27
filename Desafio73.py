10acbe3248b8b4ad9bf595c16f07673c1a1d9aac
Eduardo Bizerra <Eduardo_Bizerra@hotmail.com>
2023-02-27 16:00:56 -0300

    Initial commit

---
+++ Desafio73.py
@@ -0,0 +1,12 @@
+tabela= ('Corinthans', 'Grêmio', 'Santos',
+         'Palmeiras', 'Flamengo', 'Sport',
+         'Cruzeiro', 'Atletico PR', 'Fluminense',
+         'Coritiba', 'Botafogo', 'Vasco', 'Bahia',
+         'Ponte Preta', 'Atletico MG', 'Chapecoense',
+         'São Paulo', 'Vitoria ', 'Avai', 'Atletico Go')
+print(f'Os cinco primeiros colocados são os times {tabela [0:5]}')
+print(f'Os quatro ultimos colocados são {tabela[16:20]}')
+print(f'A ordem alfabetica da tabela é {sorted(tabela)}')
+print(f'O time da Chapecoense está na posição {tabela.index("Chapecoense")}')
+
+
