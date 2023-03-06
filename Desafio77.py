2173292c30e432abb9c49af2a4eb0fa8fd889a50
Eduardo Bizerra <Eduardo_Bizerra@hotmail.com>
2023-03-06 15:46:43 -0300

    Initial commit

---
+++ Desafio77.py
@@ -0,0 +1,8 @@
+palavras= ('Aprender', 'Programar', 'Linguagem', 'Python',
+           'Curso', 'Gratis', 'Estudar', 'praticar',
+           'trabalhar', 'mercado', 'programador', 'futuro')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
